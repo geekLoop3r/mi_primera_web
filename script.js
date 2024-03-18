d0f85022d555a7c3ff2506fecd893ab853e72e8d
geekLoop3r <hipolito.alba.c@gmail.com>
2024-03-18 17:48:24 -0500

    Barra de nav responsive con menu y form de contacto

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let nombre="Tomás"
-console.log(nombre)
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+console.log(nombre);
+
+function validarFormulario() {
+    let warnings = "";
+    let valido = true;
+    parrafo.innerHTML = "";
+
+    if (nombre.value.length < 4) {
+        warnings += ` El nombre ${nombre.value} debe contener más de 4 carcateres`;
+        valido = false;
+    }
+
+    if (!valido) {
+        parrafo.innerHTML = warnings;
+    } else {
+        parrafo.innerHTML = "Enviado";
+    }
+    return valido;
+}
+
+form.addEventListener("submit", (e) => {
+    if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+    } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+    }
+});
